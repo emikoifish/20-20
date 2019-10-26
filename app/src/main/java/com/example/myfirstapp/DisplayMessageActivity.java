9828bb9ccfc6cfc528a424a2a72e696aec3afe0c
emikoifish <emikoifish@gmail.com>
2019-10-26 14:04:21 -0700

    added notifications

--- app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -1,7 +1,10 @@
 package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -22,6 +25,7 @@ import android.widget.Toast;
 import java.util.TimerTask;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+
     public int boolCursor = 0;
     public int counter;
     public int oldCounter;
@@ -43,8 +47,6 @@ public class DisplayMessageActivity extends AppCompatActivity {
         final TextView countTime = findViewById(R.id.countTime);
         final int amountTime = Integer.parseInt(message);
         final int amountTimeMillis = amountTime * 1000;
-
-
 
         myCountDownTimer = new MyCountDownTimer(amountTimeMillis, 1000);
         myCountDownTimer.start();
@@ -69,44 +71,34 @@ public class DisplayMessageActivity extends AppCompatActivity {
                         }
                         boolCursor = 0;
                     }
-
                     oldCounter++;
+                }
+                if (counter >= amountTime) {
+                    counter = 0;
+                    boolCursor = 0;
+                    myCountDownTimer.cancel();
+                    myCountDownTimer = new MyCountDownTimer(amountTimeMillis, 1000);
+                    myCountDownTimer.start();
+                    oldCounter = 0;
                 }
                 handler.postDelayed(this, delay);
             }
         }, delay);
-
-//
-//
-//        while(true){
-//            if (counter > oldCounter) {
-//                if (isScreenOn == false) {
-//                    boolCursor++;
-//                } else { // if screen is on, then reset the amount off
-//                    if (boolCursor >= 5) {
-//                        counter = 0;
-//                        boolCursor = 0;
-//                        myCountDownTimer.cancel();
-//                        myCountDownTimer = new MyCountDownTimer(amountTimeMillis, 1000);
-//                        myCountDownTimer.start();
-//                    }
-//                    boolCursor = 0;
-//                }
-//
-//                oldCounter++;
-//            }
-//        }
-
     }
+
+
     public class MyCountDownTimer extends CountDownTimer {
+
+        public int totalTime;
 
         public MyCountDownTimer(long millisInFuture, long countDownInterval) {
             super(millisInFuture, countDownInterval);
+            totalTime = (int)millisInFuture;
         }
 
         @Override
         public void onTick(long millisUntilFinished) {
-            int seconds = (int)millisUntilFinished / 1000;
+            int seconds = totalTime/1000 - counter;
             int minutes = seconds / 60;
             seconds = seconds % 60;
             TextView countTime = findViewById(R.id.countTime);
@@ -119,8 +111,31 @@ public class DisplayMessageActivity extends AppCompatActivity {
         @Override
         public void onFinish() {
             TextView countTime = findViewById(R.id.countTime);
-            countTime.setText("Finished");
+            showNotification("Rest your eyes", "look up for 20 seconds, 20 feet away");
+//            countTime.setText("Finished");
 //            finish();
         }
+        public void showNotification(String title, String message) {
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel("20-20",
+                        "20-20",
+                        NotificationManager.IMPORTANCE_DEFAULT);
+                channel.setDescription("20-20 channel");
+                mNotificationManager.createNotificationChannel(channel);
+            }
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), "YOUR_CHANNEL_ID")
+                    .setSmallIcon(R.drawable.icon_eye) // notification icon
+                    .setContentTitle(title) // title for notification
+                    .setContentText(message)// message for notification
+                    .setAutoCancel(true); // clear notification after click
+//            Intent intent = new Intent(getApplicationContext(), ACTIVITY_NAME.class);
+//            PendingIntent pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//            mBuilder.setContentIntent(pi);
+            mNotificationManager.notify(0, mBuilder.build());
+        }
     }
+
+
 }
